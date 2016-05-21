86fc610247055dc9a2e833232084e082af00d55e
crynobone <crynobone@gmail.com>
2016-05-21 16:43:02 +0800

    Add some helper methods.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Client.php
+++ src/Client.php
@@ -37,7 +37,7 @@ class Client
      * Ping an endpoint.
      *
      * @param  string  $method
-     * @param  string  $uri
+     * @param  \Psr\Http\Message\UriInterface|string  $uri
      * @param  mixed  $data
      * @param  array  $headers
      *
@@ -45,10 +45,104 @@ class Client
      */
     public function send($method, $uri, $data = [], array $headers = [])
     {
-        $body = json_encode($data);
+        $body    = $this->prepareRequestBody($data);
+        $headers = $this->prepareRequestHeaders($headers);
 
+        return $this->client->send(strtoupper($method), $uri, $headers, $body);
+    }
+
+    /**
+     * Sends a GET request.
+     *
+     * @param  \Psr\Http\Message\UriInterface|string  $uri
+     * @param  array  $headers
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function get($uri, array $headers = [])
+    {
+        return $this->send('GET', $uri, $headers);
+    }
+
+    /**
+     * Sends a POST request.
+     *
+     * @param  \Psr\Http\Message\UriInterface|string  $uri
+     * @param  mixed  $data
+     * @param  array  $headers
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function post($uri, $data = [], array $headers = [])
+    {
+        return $this->send('POST', $uri, $data, $headers);
+    }
+
+    /**
+     * Sends a PUT request.
+     *
+     * @param  \Psr\Http\Message\UriInterface|string  $uri
+     * @param  mixed  $data
+     * @param  array  $headers
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function put($uri, $data = [], array $headers = [])
+    {
+        return $this->send('PUT', $uri, $data, $headers);
+    }
+
+    /**
+     * Sends a PATCH request.
+     *
+     * @param  \Psr\Http\Message\UriInterface|string  $uri
+     * @param  mixed  $data
+     * @param  array  $headers
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function patch($uri, $data = [], array $headers = [])
+    {
+        return $this->send('PATCH', $uri, $data, $headers);
+    }
+
+    /**
+     * Sends a DELETE request.
+     *
+     * @param  \Psr\Http\Message\UriInterface|string  $uri
+     * @param  mixed  $data
+     * @param  array  $headers
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function delete($uri, $data = [], array $headers = [])
+    {
+        return $this->send('DELETE', $uri, $data, $headers);
+    }
+
+    /**
+     * Prepare request body.
+     *
+     * @param  mixed  $data
+     *
+     * @return string
+     */
+    protected function prepareRequestBody($data = [])
+    {
+        return json_encode($data);
+    }
+
+    /**
+     * Prepare request headers.
+     *
+     * @param  array  $headers
+     *
+     * @return array
+     */
+    protected function prepareRequestHeaders(array $headers = [])
+    {
         $headers['Content-Type'] = 'application/json';
 
-        return $this->client->send(strtoupper($method)), $uri, $headers, $body);
+        return $headers;
     }
 }
