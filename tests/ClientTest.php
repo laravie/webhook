f391b91dac4012c1d5315425211225f6679de10b
Mior Muhammad Zaki <crynobone@gmail.com>
2018-12-24 11:29:54 +0800

    Improves tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -3,13 +3,14 @@
 namespace Laravie\Webhook\Tests;
 
 use Laravie\Webhook\Client;
+use Laravie\Webhook\Request;
 use PHPUnit\Framework\TestCase;
 use Laravie\Codex\Testing\Faker;
 
 class ClientTest extends TestCase
 {
     /** @test */
-    public function it_can_be_initiated()
+    public function it_has_proper_signature()
     {
         $stub = new Client(Faker::create()->http());
 
@@ -24,5 +25,28 @@ class ClientTest extends TestCase
         $stub->setContentType('application/xml');
 
         $this->assertSame('application/xml', $stub->getContentType());
+    }
+
+        /** @test */
+    public function it_can_send_api_request_on_version_one()
+    {
+        $faker = Faker::create()
+                    ->sendJson('POST', [], ['foo' => 'bar'])
+                    ->expectEndpointIs('https://acme.laravie/webhook')
+                    ->shouldResponseWith(200, 'OK');
+
+        $request = new class() extends Request {
+            public function ping()
+            {
+                return $this->send('https://acme.laravie/webhook', [], ['foo' => 'bar']);
+            }
+        };
+
+        $client = new Client($faker->http());
+
+        $response = $client->via($request)->ping();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame('OK', $response->getBody());
     }
 }
